a893594eab26eece3948fb17043ff61fd62c773a
vajki8 <vajk.olbey@gmail.com>
2024-03-05 08:26:41 +0100

    Add project files.

---
+++ workshop3/Trooper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop3
+{
+    class Trooper
+    {
+        private string name;
+        private int speed;
+        private int vitality;
+        private int cost;
+
+        public Trooper(string name, int speed, int vitality, int cost)
+        {
+            this.name = name;
+            this.speed = speed;
+            this.vitality = vitality;
+            this.cost = cost;
+        }
+
+    }
+}
