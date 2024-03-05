5ec63a7e55d0ac229f1a9161dcb78e99004270a0
vajki8 <vajk.olbey@gmail.com>
2024-03-05 09:41:52 +0100

    bugfix

--- workshop3/trooperConverter.cs
+++ workshop3/trooperConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Media;
 
 namespace workshop3
 {
-    class ProgressBar : IValueConverter
+    class trooperConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
