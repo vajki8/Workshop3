7eec9c1ef4589b1cd482d3e622636c0d08766220
BalintBihon <balint.bihon@gmail.com>
2024-03-05 09:37:17 +0100

    progressbar

--- workshop3/ProgressBar.cs
+++ workshop3/ProgressBar.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace workshop3
 {
-    class ProgressBar
+    class ProgressBar : IValueConverter
     {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int number = (int)value;
+            if (number < 4)
+            {
+                return Brushes.Red;
+            }
+            else if (number < 7)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+        }
 
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
