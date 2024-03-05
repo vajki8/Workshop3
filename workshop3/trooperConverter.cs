231d40f5c61d208878b19ae3a27a9f1bfce93586
vajki8 <vajk.olbey@gmail.com>
2024-03-05 09:45:18 +0100

    bugifx #2

--- workshop3/trooperConverter.cs
+++ workshop3/trooperConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Media;
 
 namespace workshop3
 {
-    class trooperConverter : IValueConverter
+    internal class trooperConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
