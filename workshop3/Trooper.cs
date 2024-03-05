e361e53e2e5d288e498ca73dd1244d397aa4e896
vajki8 <vajk.olbey@gmail.com>
2024-03-05 09:10:16 +0100

    trooper InotifyChanged

--- workshop3/Trooper.cs
+++ workshop3/Trooper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace workshop3
 {
-    public class Trooper
+    public class Trooper : INotifyPropertyChanged
     {
         private string name;
         private int speed;
@@ -16,23 +18,29 @@ namespace workshop3
         public string Name
         {
             get { return name; }
-            set { name = value; }
+            set { name = value; OnPropertyChanged(); }
         }
         public int Speed
         {
             get { return speed; }
-            set { speed = value; }
+            set { speed = value; OnPropertyChanged(); }
         }
         public int Vitality 
         { 
             get { return vitality; } 
-            set { vitality = value; }
+            set { vitality = value; OnPropertyChanged(); }
         }
         public int Cost
         {
             get { return cost; }
-            set { cost = value; }
+            set { cost = value; OnPropertyChanged(); }
         }
 
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if( handler != null ) { handler(this, new PropertyChangedEventArgs(propertyName));}
+        }    
     }
 }
